dcebe988d771bdf524e6fd9d58c1297c7ff660fa
mahnoor3650 <154074235+mahnoor3650@users.noreply.github.com>
2025-02-01 21:15:58 +0500

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,1 @@
-/// <reference types="vite/client" />
-/// <reference types="@remix-run/node" />
-// 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
+
